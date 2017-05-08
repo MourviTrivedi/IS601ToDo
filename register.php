61ff63fb9f18458ac086059a7180c0b13b983d27
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-05-08 14:44:31 -0400

    updated css

--- register.php
+++ register.php
@@ -1,19 +1,79 @@
 <html>
 <body>
-	<h1>This is a Register page</h1>
-	<form method = 'post' action = 'index.php'>
-	    <strong>Username: </strong> <input type='text' name='reg_uname'
-	    value="" /> </ br> </ br>
+<head>
+  <style type="text/css">
+  body {
+      background-image: url(http://wallpaperpulse.com/img/1331570.jpg);
+    }
+	div.container {
+       padding: 6px;
+	   width:400px;
+       margin:20px auto;
+	   text-align:center;
+	   background-color: #F5F4F4;
+	   border-radius: 10px;
+	   border:2px solid;
+         }
+       .signup div {
+	   border:2px solid #fff;
+       padding:3px;
+	}
+	/* .register div {
+	   border:2px solid #fff;
+	   padding:3px;
+	}*/
+	input[type=text], input[type=password] {
+	   padding: 6px 15px;
+	   margin: 8px 0;
+	   border: 1px solid #ccc;
+	   font: arial;
+	}
+	#button {
+       background-color: #8B0000;
+	   color: white;
+	   padding: 6px 15px;
+	   margin: auto;
+	   width: 83%;
+	   border-radius: 10px;
+	   font-size:medium;
+ 	}
+	#wrap {
+	   padding-top: 75px;
+	   border: 2px;
+	   border-color: black;
+ 	}
+ 	
+</style>
+</head>
 
-	    <strong>Password: </strong> <input type="password"
-	    name="reg_password" value="" /> </ br> </ br>
-	    <input type="hidden" name="action" value="register" />
-	    <br>
-	    <input type="submit" value="Register" />
-	</form>
+<div id="wrap">
+<div class='container'>
+<form action="index.php" method="POST">
+<h2>Register</h2>
+<table align="center">
+   <tbody>
+     <tr><td><label><strong>First Name:</strong></label></td><td><input name="first_name" type="text"></td></tr>
+      <tr><td><label><strong>Last Name:</strong></label></td><td><input name="last_name" type="text"></td></tr>
+       <tr><td><label><strong>Username:</strong></label></td><td><input name="uname" type="text"></td></tr>
+       <tr><td><label><strong>Password:</strong></label></td><td><input name="password" type="password"></td></tr>
+       <tr><td><label><strong>E-mail Address:</strong></label></td><td><input name="email" type = "text"></td></tr>
+       <tr><td><label><strong>Phone Number:</strong></label></td><td><input name="phonenumber" type = "text"></td></tr>
+       <tr><td><label><strong>Birthday:</strong></label></td><td><input name="birthday" type = "date"></td></tr>
+       <tr>
+<td><strong>Gender:</strong></td>
+<td><input required="true" id="male" name="g" value="male" type="radio" /><label="male">Male</label>&nbsp;&nbsp;
+<input required="true" id="female" name="g" value="female" type="radio"><label="female">Female</label></td>
+</tr>
+  </tbody>
+  </table>
+<br />
+<div id="button" align="center">
+<input type="submit">
+<input type="hidden" name="action" value="register">
+</div>
+</form>
 
-	<form action="login.php" method = 'post'>
-	  <input type='submit' value='try to login' />
-	</form>
+</div>
+</div>
 </body>
 </html>
