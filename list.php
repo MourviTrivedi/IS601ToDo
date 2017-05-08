412303a6e41ebd73990bf622b5c4db4217d279a6
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-05-08 16:32:46 -0400

    added css

--- list.php
+++ list.php
@@ -1,3 +1,23 @@
+<html>
+<head>
+  <title>To Do List</title>
+  <style type="text/css">
+    body {
+      background-image: url(http://wallpaperpulse.com/img/1331570.jpg);
+    }
+    #list {
+      padding: 6px;
+      width:300px;
+      margin:20px auto;
+      text-align:center; 
+      background-color: #E1DFDF;
+      border-radius: 10px;
+      border:2px solid; 
+    }
+    </style>
+  </head>
+<body>
+  <div id='list'>
 <?php
 echo "<h1>To-Do List System</h1><br/>";
 echo "Welcome, ".$_COOKIE['login'].'<br />';
@@ -15,10 +35,10 @@ echo "</ br> </ br>";
 	 <tr>
 	   <td> <?php echo $res['todo_item']; ?> </td>
 	   <td>
-	     <form action='index.php' method='POST'>
+	     <form action='index.php' method="POST">
 	      <input type='hidden' name='item_id' value= <?php echo $res['id']; ?> >
 	      <input type='hidden' name='action' value='delete'>
-	      <input type='submit' value="delete"/> 
+	      <input type="submit" value="delete"/> 
 	     </form>
 	      </td>
 		<td>
@@ -37,9 +57,8 @@ echo "</ br> </ br>";
 	   <input type='text' name='description'/><br>
 	   <input type='hidden' name = 'action' value='add'><br>
 	   <input type="submit" value="Add"/>
-
-
-        </form>
-
-  </body>
+	</form>
+	
+</div>
+</body>
 </html>
