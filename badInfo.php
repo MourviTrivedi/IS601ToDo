3993fa6f4385726100d78a57cc2bbccd27416384
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-05-08 22:32:01 -0400

    changed css

--- badInfo.php
+++ badInfo.php
@@ -1,3 +1,38 @@
+<html>
+<head>
+  <style type="text/css">
+  body {
+      background-image: url(http://wallpaperpulse.com/img/1331570.jpg);
+    }
+  #wrap-id {
+	    background-color: #8B0000;
+      color: white;
+      padding: 6px 15px;
+      margin: auto;
+      width: 50%;
+      border-radius: 10px;
+      font-size:medium;
+      padding-top: 100px;
+    }
+    .button {
+      background-color: #8B0000;
+      color: white;
+      padding: 6px 15px;
+      margin: 8px 0;
+      width: 83%;
+      border-radius: 10px;
+      font-size:medium;
+    }
+ </style>
+ </head>
+<body>
+<div id="wrap-id" align="center">
 <?php
-  echo "Wrong Username or Password, try again!";
-?>
+  echo "Wrong Username or Password!!";
+  ?>
+  <form action="login.php" class="button">
+  <button type="submit">Try Again</button>
+</div>
+
+</body>
+</html>
