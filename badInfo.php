1ed7bf6390ee1d8fff4cdbcf43fc6395ebe4a2c5
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-05-08 23:38:34 -0400

    updated css

--- badInfo.php
+++ badInfo.php
@@ -4,34 +4,36 @@
   body {
       background-image: url(http://wallpaperpulse.com/img/1331570.jpg);
     }
-  #wrap-id {
-	    background-color: #8B0000;
-      color: white;
-      padding: 6px 15px;
+  .wrap-id {
+	    background-color: #E1DFDF;
+      color: #000000;
       margin: auto;
-      width: 50%;
+      width: 35%;
       border-radius: 10px;
       font-size:medium;
-      padding-top: 100px;
+      padding-top: 10px;
+      margin-top:250px;
+      font-size: 25px; 
     }
-    .button {
+    #button {
       background-color: #8B0000;
       color: white;
       padding: 6px 15px;
       margin: 8px 0;
-      width: 83%;
+      width: 40%;
       border-radius: 10px;
       font-size:medium;
     }
  </style>
  </head>
 <body>
-<div id="wrap-id" align="center">
+<div class="wrap-id" align="center">
 <?php
-  echo "Wrong Username or Password!!";
+  echo "Wrong Username or Password!";
   ?>
   <form action="login.php" class="button">
-  <button type="submit">Try Again</button>
+  <button id="button">Try Again</button>
+
 </div>
 
 </body>
